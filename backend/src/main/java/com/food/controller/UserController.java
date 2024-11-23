fad15df404385cb382b593ce985dc06a7c30de20
yangjunsik <wnstlr1379@naver.com>
2024-11-23 15:26:05 +0900

    마지막
    일수도

--- backend/src/main/java/com/food/controller/UserController.java
+++ backend/src/main/java/com/food/controller/UserController.java
@@ -5,6 +5,7 @@ import com.food.config.JwtUtil;
 import com.food.dto.UserDTO;
 import com.food.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
